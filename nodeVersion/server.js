d792ed4c1c427ea33fb1d41d93e35f450ad874ff
Elvin Samuel <elvinsamuel1@gmail.com>
2018-05-07 18:16:58 +0000

    Added 'server.js' file, and inital base route. Used vim to edit.

---
+++ nodeVersion/server.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var app = express();
+
+// Insert some simple routes here:
+app.use(express.static(__dirname + '/public'))
+
+app.get('/', function(req, res){
+	res.sendFile('index');
+});
+
+app.listen(process.env.PORT, process.env.IP, function(){
+	console.log(`All-In-One Server Up & Running On Port ${process.env.PORT}`);
+});
