7e3e0c8273639ed5ad5e62eb59349699eb902265
Elvin Samuel <elvinsamuel1@gmail.com>
2018-05-14 00:59:51 +0000

    server.js modified, also adding some recovered Vim files for server.js

--- nodeVersion/server.js
+++ nodeVersion/server.js
@@ -1,8 +1,13 @@
 var express = require('express');
 var app = express();
+var sass = require('node-sass');
 
 // Insert some simple routes here:
-app.use(express.static(__dirname + '/public'))
+app.use(express.static(__dirname + '/public'));
+
+// Implementing Sass
+sass.render({ file: scss_filename }, function(err, result) {});
+var result = sass.renderSync({ data: scss_content });
 
 app.get('/', function(req, res){
 	res.sendFile('index');
