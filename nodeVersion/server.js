6bcf0c9e2e5a0e03740d9e3c9018bccc2fda4b7c
Elvin Samuel <elvinsamuel1@gmail.com>
2018-05-24 14:59:36 +0000

    Removed the sass implementation from server.js and tested index.html to make sure it's working

--- nodeVersion/server.js
+++ nodeVersion/server.js
@@ -1,16 +1,10 @@
 var express = require('express');
 var app = express();
-var sass = require('node-sass');
-
 // Insert some simple routes here:
 app.use(express.static(__dirname + '/public'));
 
-// Implementing Sass
-sass.render({ file: scss_filename }, function(err, result) {});
-var result = sass.renderSync({ data: scss_content });
-
 app.get('/', function(req, res){
-	res.sendFile('index');
+	res.sendFile('index.html');
 });
 
 app.listen(process.env.PORT, process.env.IP, function(){
